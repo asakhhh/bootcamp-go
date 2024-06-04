b070c0126f82a1140e3cc4ba50fc9483d0fe12ef
asakhant <asakhant@noreply.platform.alem.school>
2024-06-04 07:59:19 +0000

    Update unixbox/09.sh

--- unixbox/09.sh
+++ unixbox/09.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+sz=$(wc -l poem.txt)
+num1=$(expr 1 + $RANDOM % $sz)
+num2=$(expr 1 + $RANDOM % $sz)
+echo $(head -n $num1 poem.txt | tail -1)
+echo $(head -n $num2 poem.txt | tail -1)
