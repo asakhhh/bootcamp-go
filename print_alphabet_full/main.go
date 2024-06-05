3d2b0face557f349382e64f06e72fa2fcdacf724
asakhhh <asakhantayev@gmail.com>
2024-06-05 19:10:42 +0600

    alphafull

---
+++ print_alphabet_full/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/alem-platform/ap"
+
+func main() {
+	for i := 0; i < 26; i++ {
+		ap.PutRune(rune('a' + i))
+		ap.PutRune(rune('A' + i))
+	}
+	ap.PutRune('\n')
+}
