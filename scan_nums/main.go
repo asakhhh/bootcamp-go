e4189049680e5a773b45f269ea3fc3b8cde1e450
adi <asakhantayev@gmail.com>
2024-06-06 15:50:26 +0600

    scanums

--- scan_nums/main.go
+++ scan_nums/main.go
@@ -10,8 +10,8 @@ func main() {
 	fmt.Scanf("%d %d", &num1, &num2)
 
 	if num2 == 0 {
-		fmt.Printf("%d %d %d F\n", num1+num2, num1-num2, num1*num2)
+		fmt.Printf("%d %d %d F", num1+num2, num1-num2, num1*num2)
 	} else {
-		fmt.Printf("%d %d %d %d\n", num1+num2, num1-num2, num1*num2, num1/num2)
+		fmt.Printf("%d %d %d %d", num1+num2, num1-num2, num1*num2, num1/num2)
 	}
 }
