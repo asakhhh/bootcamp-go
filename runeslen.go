87696afa7fb72d067f30e06125e592ec703e7fa0
adi <asakhantayev@gmail.com>
2024-06-06 16:34:25 +0600

    runeslen

--- runeslen.go
+++ runeslen.go
@@ -1,13 +1,19 @@
 package bootcamp
 
-import "github.com/alem-platform/ap"
+// import "fmt"
 
 func RunesLen(arr [20]rune) int {
 	c := 0
-	
-	for c < 20 && arr[c] != '\0' {
+
+	for c < 20 && arr[c] != 0 {
 		c++
 	}
 
 	return c
 }
+
+// func main() {
+// 	arr := [20]rune{'a', 'b', 'c', '\000', 'e'}
+
+// 	fmt.Printf("%d", RunesLen(arr))
+// }
