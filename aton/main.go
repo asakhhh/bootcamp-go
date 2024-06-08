7b3f32e30b861c42f221dac01db680e144c0d6c7
asakhant <asakhant@noreply.platform.alem.school>
2024-06-08 12:50:07 +0000

    Add aton/main.go

---
+++ aton/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+
+	var r rune
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%c", &r)
+		if r != '\n' {
+			fmt.Printf("%d", r)
+			if i != n-1 {
+				fmt.Printf(" ")
+			}
+		}
+	}
+}
