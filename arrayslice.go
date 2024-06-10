9b24fe91cc9d00814e31388453423d68cf4a47a1
asakhant <asakhantayev@gmail.com>
2024-06-10 15:08:37 +0600

    arrayslice

---
+++ arrayslice.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func ArraySlice(arr [20]int, low int, high int) []int {
+	var a []int
+	for i := low; i < high; i++ {
+		a = append(a, arr[i])
+	}
+	return a
+}
+
+func main() {
+	arr := [20]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(ArraySlice(arr, 3, 5)) // [3, 4]
+	fmt.Println(ArraySlice(arr, 5, 5)) // []
+	fmt.Println(ArraySlice(arr, 5, 1)) // []
+}
