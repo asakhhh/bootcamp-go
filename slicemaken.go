5db058ef0b63afb08ad0513a887ca27ac1b99329
asakhant <asakhantayev@gmail.com>
2024-06-10 15:17:56 +0600

    slicemaken

---
+++ slicemaken.go
@@ -0,0 +1,18 @@
+package bootcamp
+
+// import "fmt"
+
+func SliceMakeN(n int) []int {
+	a := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		a[i] = i
+	}
+
+	return a
+}
+
+// func main() {
+// 	fmt.Println(SliceMakeN(5))  // [0, 1, 2, 3, 4]
+// 	fmt.Println(SliceMakeN(10)) // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// }
