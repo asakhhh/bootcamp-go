a994b49a68973e29297eaf82f3b1654f93ad68a5
asakhant <asakhantayev@gmail.com>
2024-06-10 17:18:24 +0600

    slicematrixprint

--- slicematrixprint.go
+++ slicematrixprint.go
@@ -3,14 +3,14 @@ package bootcamp
 import "github.com/alem-platform/ap"
 
 func SliceMatrixPrint(matrix [][]rune) {
-	for _, v := range matrix {
+	for i, v := range matrix {
 		for i := 0; i < len(v); i++ {
 			ap.PutRune(v[i])
 			if i != len(v)-1 {
 				ap.PutRune(' ')
 			}
 		}
-		if _ != len(matrix)-1 {
+		if i != len(matrix)-1 {
 			ap.PutRune('\n')
 		}
 	}
