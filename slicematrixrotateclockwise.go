6a6589602c5991dbd1fb086045055893354fdc59
asakhant <asakhantayev@gmail.com>
2024-06-11 15:39:51 +0600

    slicematrixrotateclockwise.go

---
+++ slicematrixrotateclockwise.go
@@ -0,0 +1,34 @@
+package bootcamp
+
+func SliceMatrixRotateClockwise(matrix [][]rune) {
+	n := len(matrix)
+	a := make([][]rune, n)
+	for i := 0; i < n; i++ {
+		a[i] = make([]rune, n)
+		for j := 0; j < n; j++ {
+			a[i][j] = matrix[n-1-j][i]
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		copy(matrix[i], a[i])
+	}
+}
+
+// func main() {
+// 	matrix := [][]rune{
+// 		{'a', 'b', 'c'},
+// 		{'d', 'e', 'f'},
+// 		{'g', 'h', 'i'},
+// 	}
+
+// 	fmt.Println(matrix)
+
+// 	SliceMatrixRotateClockwise(matrix)
+
+// 	fmt.Println(matrix)
+// 	// Output:
+// 	// g d a
+// 	// h e b
+// 	// i f c
+// }
