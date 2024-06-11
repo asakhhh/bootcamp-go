3bf5eef46735353190368dc4f60b2ad4cc1854a1
asakhant <asakhantayev@gmail.com>
2024-06-11 16:28:15 +0600

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package bootcamp
 
 func IsAlpha(s string) bool {
 	for _, v := range s {
@@ -11,8 +9,8 @@ func IsAlpha(s string) bool {
 	return true
 }
 
-func main() {
-	fmt.Println(IsAlpha("h["))          // false
-	fmt.Println(IsAlpha("Hello123"))    // false
-	fmt.Println(IsAlpha("Hello World")) // false
-}
+// func main() {
+// 	fmt.Println(IsAlpha("h["))          // false
+// 	fmt.Println(IsAlpha("Hello123"))    // false
+// 	fmt.Println(IsAlpha("Hello World")) // false
+// }
