5070aaa2664a152e497f868065e8512baa4131a1
asakhant <asakhantayev@gmail.com>
2024-06-11 16:15:03 +0600

    rotaten

--- slicematrixrotateclockwise.go
+++ slicematrixrotateclockwise.go
@@ -1,25 +1,5 @@
 package bootcamp
 
-func SliceMatrixRotateClockwise(matrix [][]rune) {
-	n := len(matrix)
-
-	if n == 0 || n != len(matrix[0]) {
-		return
-	}
-
-	a := make([][]rune, n)
-	for i := 0; i < n; i++ {
-		a[i] = make([]rune, n)
-		for j := 0; j < n; j++ {
-			a[i][j] = matrix[n-1-j][i]
-		}
-	}
-
-	for i := 0; i < n; i++ {
-		copy(matrix[i], a[i])
-	}
-}
-
 // func main() {
 // 	matrix := [][]rune{
 // 		{'a', 'b', 'c'},
--- slicematrixrotaten.go
+++ slicematrixrotaten.go
@@ -1,5 +1,25 @@
 package bootcamp
 
+func SliceMatrixRotateClockwise(matrix [][]rune) {
+	n := len(matrix)
+
+	if n == 0 || n != len(matrix[0]) {
+		return
+	}
+
+	a := make([][]rune, n)
+	for i := 0; i < n; i++ {
+		a[i] = make([]rune, n)
+		for j := 0; j < n; j++ {
+			a[i][j] = matrix[n-1-j][i]
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		copy(matrix[i], a[i])
+	}
+}
+
 func SliceMatrixRotateN(matrix [][]rune, turns int) {
 	turns = (turns%4 + 4) % 4
 
