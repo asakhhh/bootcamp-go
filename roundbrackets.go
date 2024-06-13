73fa96921587208dce6243cd14b947ba97caa473
asakhant <asakhantayev@gmail.com>
2024-06-13 17:07:18 +0600

    brackets

---
+++ roundbrackets.go
@@ -0,0 +1,17 @@
+package bootcamp
+
+func RoundBrackets(s string) bool {
+	c := 0
+
+	for _, v := range s {
+		if v == '(' {
+			c++
+		} else if v == ')' {
+			c--
+			if c < 0 {
+				return false
+			}
+		}
+	}
+	return c == 0
+}
