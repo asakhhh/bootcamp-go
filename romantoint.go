35ff675b03415bb20e32b837840c64bade932966
asakhant <asakhantayev@gmail.com>
2024-06-17 15:33:33 +0600

    story7/15

---
+++ romantoint.go
@@ -0,0 +1,47 @@
+package bootcamp
+
+func RomanToInt(s string) int {
+	m := map[string]int{
+		"I":    1,
+		"II":   2,
+		"III":  3,
+		"IV":   4,
+		"V":    5,
+		"VI":   6,
+		"VII":  7,
+		"VIII": 8,
+		"IX":   9,
+		"X":    10,
+		"XX":   20,
+		"XXX":  30,
+		"XL":   40,
+		"L":    50,
+		"LX":   60,
+		"LXX":  70,
+		"LXXX": 80,
+		"XC":   90,
+		"C":    100,
+		"CC":   200,
+		"CCC":  300,
+		"CD":   400,
+		"D":    500,
+		"DC":   600,
+		"DCC":  700,
+		"DCCC": 800,
+		"CM":   900,
+		"M":    1000,
+		"MM":   2000,
+		"MMM":  3000,
+	}
+
+	num := 0
+	current_pos := 3
+
+	for i, c := range s {
+		v, ok := m[string(c)]
+		if !ok {
+			return 0
+		}
+		if i + 2 <= len(s)
+	}
+}
