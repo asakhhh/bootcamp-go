149a0f86e90731f16599a216c67f6a20e1551d0f
asakhant <asakhantayev@gmail.com>
2024-06-18 17:52:20 +0600

    story8/7

--- my_xxd/main.go
+++ my_xxd/main.go
@@ -125,7 +125,7 @@ func main() {
 			}
 			ap.PutRune(' ')
 		}
-		for x := 0; x < 2*(i+c-j)+(i+c-j+1)/2; x++ {
+		for x := 0; x < 2*(i+c-j)+(i+c-j)/2; x++ {
 			ap.PutRune(' ')
 		}
 		PrintString(" " + NoNewline(s[i:j]) + "\n")
