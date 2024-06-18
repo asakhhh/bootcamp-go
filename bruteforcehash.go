f25a3fc22c3bb3ae6acc8288032c2bb04d2b5ac3
asakhant <asakhantayev@gmail.com>
2024-06-18 20:19:53 +0600

    bfh

--- bruteforcehash.go
+++ bruteforcehash.go
@@ -1,15 +1,59 @@
-package bootcamp
+package main
 
 import (
 	"crypto/md5"
-	"encoding/hex"
+	"fmt"
 )
 
-func BruteForceHash(hash string) string {
-	// Define characters for brute force
-	characters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	charsetLen := len(characters)
+func hexToByte(c byte) byte {
+	if '0' <= c && c <= '9' {
+		return c - '0'
+	}
+	if 'a' <= c && c <= 'f' {
+		return c - 'a' + 10
+	}
+	return c - 'A' + 10
+}
 
+// hexStringToByteArray converts a valid hex string to a [16]byte array.
+func hexStringToByteArray(hexStr string) [16]byte {
+	var result [16]byte
+	for i := 0; i < 16; i++ {
+		highNibble := hexToByte(hexStr[2*i])
+		lowNibble := hexToByte(hexStr[2*i+1])
+		result[i] = (highNibble << 4) | lowNibble
+	}
+	return result
+}
+
+func BruteForceHash1(targetHash [16]byte) string {
+	const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	maxLength := 6
+
+	var helper func(prefix string, length int) string
+	helper = func(prefix string, length int) string {
+		if length > maxLength {
+			return ""
+		}
+
+		h := md5.Sum([]byte(prefix))
+		if h == targetHash {
+			return prefix
+		}
+
+		for _, c := range chars {
+			result := helper(prefix+string(c), length+1)
+			if result != "" {
+				return result
+			}
+		}
+		return ""
+	}
+
+	return helper("", 0)
+}
+
+func BruteForceHash(hash string) string {
 	if len(hash) != 32 {
 		return ""
 	}
@@ -19,45 +63,12 @@ func BruteForceHash(hash string) string {
 		}
 	}
 
-	// Iterate over all possible combinations up to length 6
-	for i := 0; i <= 6; i++ {
-		// Generate all strings of length i using characters
-		result := make([]byte, i)
-		if generateString(hash, result, 0, characters, charsetLen) {
-			return string(result)
-		}
-	}
-
-	return "" // If no match found
+	return BruteForceHash1(hexStringToByteArray(hash))
 }
 
-func generateString(targetHash string, result []byte, pos int, characters string, charsetLen int) bool {
-	if pos == len(result) {
-		// Convert result to string and compute MD5 hash
-		md5Hash := md5.Sum([]byte(result))
-		computedHash := hex.EncodeToString(md5Hash[:])
-
-		// Compare computed hash with target hash
-		if computedHash == targetHash {
-			return true
-		}
-		return false
-	}
-
-	// Try all possible characters for current position
-	for i := 0; i < charsetLen; i++ {
-		result[pos] = characters[i]
-		if generateString(targetHash, result, pos+1, characters, charsetLen) {
-			return true
-		}
-	}
-
-	return false
+func main() {
+	fmt.Println(BruteForceHash("ab6ccd17455d5347c49606d641e0b2af")) // SALEM
+	fmt.Println(BruteForceHash("3cbfa33db66b830bfcf47ecc956505f8")) // ALEM
+	fmt.Println(BruteForceHash(""))                                 //
+	fmt.Println(BruteForceHash("abc"))                              //
 }
-
-// func main() {
-// 	fmt.Println(BruteForceHash("ab6ccd17455d5347c49606d641e0b2af")) // SALEM
-// 	fmt.Println(BruteForceHash("3cbfa33db66b830bfcf47ecc956505f8")) // ALEM
-// 	fmt.Println(BruteForceHash(""))                                 //
-// 	fmt.Println(BruteForceHash("abc"))                              //
-// }
