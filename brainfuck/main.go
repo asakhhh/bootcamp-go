1a81c869090bb0e57e1689783d9a7e82d81e2446
asakhant <asakhantayev@gmail.com>
2024-06-21 00:38:58 +0600

    brainfuck

--- brainfuck/main.go
+++ brainfuck/main.go
@@ -1,5 +1,87 @@
 package main
 
-// func main() {
-// 	a := make([]byte)
-// }
+import (
+	"os"
+
+	"github.com/alem-platform/ap"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		ap.PutRune('\n')
+		return
+	}
+
+	s := os.Args[1]
+	ptr := make([]byte, 1)
+	ptr[0] = 0
+	p := 0
+	i := 0
+	for i < len(s) {
+		if s[i] == ']' {
+			if ptr[p] == 0 {
+				i++
+				continue
+			}
+			cnt := 1
+			found := false
+			for j := i - 1; j >= 0; j-- {
+				if s[j] == ']' {
+					cnt++
+				} else if s[j] == '[' {
+					cnt--
+					if cnt == 0 {
+						i = j
+						found = true
+						break
+					}
+				}
+			}
+			if !found {
+				i++
+			}
+		} else if s[i] == '[' {
+			if ptr[p] != 0 {
+				i++
+				continue
+			}
+			cnt := 1
+			found := false
+			for j := i + 1; j < len(s); j++ {
+				if s[j] == '[' {
+					cnt++
+				} else if s[j] == ']' {
+					cnt--
+					if cnt == 0 {
+						i = j
+						found = true
+						break
+					}
+				}
+			}
+			if !found {
+				i++
+			}
+		} else if s[i] == '>' {
+			p++
+			if p == len(ptr) {
+				ptr = append(ptr, byte(0))
+			}
+			i++
+		} else if s[i] == '<' {
+			i++
+			p--
+		} else if s[i] == '+' {
+			i++
+			ptr[p]++
+		} else if s[i] == '-' {
+			i++
+			ptr[p]--
+		} else if s[i] == '.' {
+			ap.PutRune(rune(ptr[p]))
+			i++
+		} else {
+			i++
+		}
+	}
+}
