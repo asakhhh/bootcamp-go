348f63d5f3aa44a374f120ff90fd4f8315d0c699
asakhant <asakhantayev@gmail.com>
2024-06-20 18:08:15 +0600

    nthrune

--- testnthrune.go
+++ testnthrune.go
@@ -1,14 +1,7 @@
 package bootcamp
 
-import "math/rand"
-
 func TestNthRune(fn func(s string, n int) rune) bool {
-	var s string
-	n := int(rand.Int31n(1000) + 1)
-
-	for i := 0; i < n; i++ {
-		s += string(byte(rand.Int31n(256)))
-	}
+	s := "abacabac11"
 
 	r := []rune(s)
 	for i := 0; i < len(r); i++ {
