d50c518f84435a73ba0f881640a01e134f6e9160
asakhant <asakhantayev@gmail.com>
2024-06-24 21:00:57 +0600

    story10/6

---
+++ lockstates.go
@@ -0,0 +1,25 @@
+package bootcamp
+
+type Lock struct {
+	isLocked bool
+}
+
+func (l Lock) Lock() bool {
+	if l.isLocked {
+		return false
+	}
+	l.isLocked = true
+	return true
+}
+
+func (l Lock) Unlock() bool {
+	if !l.isLocked {
+		return false
+	}
+	l.isLocked = false
+	return true
+}
+
+func (l Lock) IsLocked() bool {
+	return l.isLocked
+}
