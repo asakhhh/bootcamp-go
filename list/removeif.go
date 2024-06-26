e4926cf3e818471df4cdf761d3a830c605198e38
asakhant <asakhantayev@gmail.com>
2024-06-26 18:36:50 +0600

    11/13

---
+++ list/removeif.go
@@ -0,0 +1,23 @@
+package bootcamp
+
+func (l *List) RemoveIf(fn func(n *ListNode) bool) {
+	curNode := l.Head
+	var prevNode *ListNode
+
+	for curNode != nil {
+		if fn(curNode) {
+			nextNode := curNode.Next
+			if prevNode != nil {
+				prevNode.Next = nextNode
+			}
+			if l.Head == curNode {
+				l.Head = l.Head.Next
+			}
+			if l.Tail == curNode {
+				l.Tail = prevNode
+			}
+		} else {
+			prevNode = curNode
+		}
+	}
+}
