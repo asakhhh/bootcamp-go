e4149e6299d2e4b1b08756a12407501c9975ff85
asakhant <asakhantayev@gmail.com>
2024-06-26 22:37:28 +0600

    12/20

--- levels_btree.go
+++ levels_btree.go
@@ -2,14 +2,21 @@ package bootcamp
 
 import btree "bootcamp/btree"
 
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
 func LevelsBTreeNode(n *btree.BTreeNode, lev int) int {
 	if n.Left == nil && n.Right == nil {
 		return lev
 	}
 	if n.Left != nil {
-		return LevelsBTreeNode(n.Left, lev+1)
+		lev = max(lev, LevelsBTreeNode(n.Left, lev+1))
 	}
-	return LevelsBTreeNode(n.Right, lev+1)
+	return max(lev, LevelsBTreeNode(n.Right, lev+1))
 }
 
 func LevelsBtree(b *btree.BTree) int {
